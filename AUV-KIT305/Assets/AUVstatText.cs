c04c9fdfa6a4f4bb618a289b865f6343d8fc6dc7
Ted Krimmer <83988764+Tarnsky@users.noreply.github.com>
2022-05-12 16:26:58 +1000

    Rotated AUV + AUVstats face the camera Script

---
+++ AUV-KIT305/Assets/AUVstatText.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AUVstatText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraToLookAt = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(cameraToLookAt.transform);
+        transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
+    }
+}
